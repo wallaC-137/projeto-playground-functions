ead4251feb3e0db859ec221cff4b6735e6893bf8
Wallace <wallace_031@hotmail.com>
2022-09-27 18:24:58 -0300

    function catAndMouse feita

--- src/challenges.js
+++ src/challenges.js
@@ -17,45 +17,45 @@ function splitSentence(param) {
 // Desafio 4
 let array = ['foguete', 'não', 'tem', 'ré']
 function concatName(param) {
-  let newArray = param[param.length-1]+ ', ' + param[0]
+  let newArray = param[param.length - 1] + ', ' + param[0]
   return newArray
 }
 // Desafio 5
 
 function footballPoints(wins, ties) {
- return (wins *3) + ties
+  return (wins * 3) + ties
 }
 
 // Desafio 6
-// let num = [9, 1, 2, 3, 9, 5, 7];
-// function highestCount (param) {
-//   let highted = 0;
+let num = [9, 1, 2, 3, 9, 5, 7];
+function highestCount (param) {
+  let highted = 0;
 
-//   for(let index = 0; index < param.length; index += 1){
-//     let count = 0;
+  for(let index = 0; index < param.length; index += 1){
+    let count = 0;
 
-//     for(let index2 = 0; index2 < param.length; index2 += 1){
-//       if(param[index] == param[index2]){
-//         count += 1;
-//       }
-//     }
-//     if(param[index] > highted){
-//       highted = count;
-//     }
-//   }
-//   return highted
-// }
-// console.log(highestCount(num))
+    for(let index2 = 0; index2 < param.length; index2 += 1){
+      if(param[index] == param[index2]){
+        count += 1;
+      }
+    }
+    if(param[index] > highted){
+      highted = count;
+    }
+  }
+  return highted
+}
+console.log(highestCount(num))
 
 // Desafio 7
 
 function catAndMouse(param1, param2, param3) {
-  if(param2 < param3){ 
-    return 'cat1'
-  } else if(param3 < param2){
-    return 'cat2'
-  } else if (param2 == param1 && param3 == param1 ){
-    return 'os gatos trombam e o rato foge'
+  if (param2 < param3) {
+    return 'cat1';
+  } else if (param3 < param2) {
+    return 'cat2';
+  } else if (param2 === param1 && param3 === param1) {
+    return 'os gatos trombam e o rato foge';
   }
 }
 
